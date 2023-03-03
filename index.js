f4c5866c28cb745d79c6691c827e00f8565c739b
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2023-03-03 17:03:07 +0530

    create contact and display it

--- index.js
+++ index.js
@@ -4,8 +4,39 @@ const app = express();
 const db = require('./config/mongoose');
 const contact = require('./model/contacts');
 
+app.use(express.urlencoded());
+app.use(express.static('./assets'));
+app.set('view engine','ejs');
+app.set('views','views');
+
+// for display all the contact (fatching from db).
 app.get('/',function(req,res){
-    res.send('Hello Shubham');    
+    contact.find({},function(err,allContact){
+        if(err){
+            console.log('error in fatching contact from db');
+            return err;
+        }
+        return res.render('home',{
+            title: 'Home Page',
+            contact : allContact
+        });
+    })  
+})
+
+// create new contact
+app.post('/create-contact',function(req,res){
+    contact.create({
+        name : req.body.name,
+        phone : req.body.phone
+    },
+    function(err,newContact){
+        if(err){
+            console.log('Error in creating a contact');
+            return err;
+        }
+        console.log('New Contact Created!!');
+        return res.redirect('back');
+    })
 })
 
 app.listen(port,function(err){
